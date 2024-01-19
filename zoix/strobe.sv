fb415596f7d9913511f8614a9fee60799893c974
Fabri <fabricava2000@gmail.com>
2024-01-19 02:15:44 +0100

    Results Golden and SBST program

--- zoix/strobe.sv
+++ zoix/strobe.sv
@@ -30,13 +30,13 @@ initial begin
 
         //OUTPUTS
 
-                //$fs_strobe(`TOPLEVEL.instr_req_o);
-                //$fs_strobe(`TOPLEVEL.data_req_o);
-                //$fs_strobe(`TOPLEVEL.data_we_o);
-                //$fs_strobe(`TOPLEVEL.instr_addr_o);
-                //$fs_strobe(`TOPLEVEL.data_addr_o);
-                //$fs_strobe(`TOPLEVEL.data_wdata_o);
-                //$fs_strobe(`TOPLEVEL.data_be_o);
+                $fs_strobe(`TOPLEVEL.instr_req_o);
+                $fs_strobe(`TOPLEVEL.data_req_o);
+                $fs_strobe(`TOPLEVEL.data_we_o);
+                $fs_strobe(`TOPLEVEL.instr_addr_o);
+                $fs_strobe(`TOPLEVEL.data_addr_o);
+                $fs_strobe(`TOPLEVEL.data_wdata_o);
+                $fs_strobe(`TOPLEVEL.data_be_o);
 
                 //ALU_TEST
                 //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.alu_i.result_o);
@@ -44,10 +44,10 @@ initial begin
                 //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.alu_i.ready_o);
 
                 //MULT_TEST
-                $fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.result_o);
-                $fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.multicycle_o);
-                $fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.mulh_active_o);
-                $fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.ready_o);
+                //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.result_o);
+                //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.multicycle_o);
+                //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.mulh_active_o);
+                //$fs_strobe(`TOPLEVEL.core_i.ex_stage_i.mult_i.ready_o);
 
                 //COMPRESSED_DECODER_TEST
                 //$fs_strobe(`TOPLEVEL.core_i.if_stage_i.compressed_decoder_i.instr_o);
@@ -56,10 +56,10 @@ initial begin
 
                 //INTERRUPT_CONTROLLER_TEST
                 //$fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_req_ctrl_o);
-                //$fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_sec_ctrl_o);
-                //$fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_id_ctrl_o);
-                //$fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_wu_ctrl_o);
-                //$fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.mip_o);
+                //fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_sec_ctrl_o);
+                //fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_id_ctrl_o);
+                //fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.irq_wu_ctrl_o);
+                //fs_strobe(`TOPLEVEL.core_i.id_stage_i.int_controller_i.mip_o);
 
 
                 #10000; // TMAX Strobe period
